ecf72e3e195dd945da55da2159e99fade3cdb723
Jashwanth Bheemapaka <65611955+jashwanth999@users.noreply.github.com>
2021-05-14 11:28:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from flask_cors import CORS, cross_origin
 import requests
 import json
 from tmdbv3api import TMDb
+import pickle as pkl
 import numpy as np
 tmdb=TMDb()
 tmdb.api_key='8b5da40bcd2b5fa4afe55c468001ad8a'
@@ -15,7 +16,32 @@ tmdb_movie=Movie()
 df2=pd.read_csv("Main_data.csv")
 from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
-
+vectorizer=pkl.load(open('vectorizerer.pkl', 'rb'))
+clt=pkl.load(open('nlp_model.pkl', 'rb'))
+def getreview(x):
+    data=[]
+    result=tmdb_movie.search(x)
+    movie_id=result[0].id
+    response=requests.get("https://api.themoviedb.org/3/movie/{}/reviews?api_key=8b5da40bcd2b5fa4afe55c468001ad8a&language=en-US&page=1".format(movie_id))
+    data_json=response.json()
+    data.append(data_json)
+    return data
+def getrating(title):
+    movie_review = []
+    data=getreview(title)
+    for i in data[0]['results']:
+        pred=clt.predict(vectorizer.transform([i['content']]))
+        if pred[0]=='positive':
+            movie_review.append({
+                "review":i['content'],
+                "rating":"Good"
+            })
+        else:
+            movie_review.append({
+                "review": i['content'],
+                "rating": "Bad"
+            })
+    return movie_review
 def get_data(x):
     data=[]
     result=tmdb_movie.search(x)
@@ -97,6 +123,10 @@ def getnames():
 def getmovie(movie_name):
    data=get_data2(movie_name)
    return jsonify(data[0])
+@app.route('/getreview/<movie_name>', methods=["GET"])
+def getreviews(movie_name):
+    data=getrating(movie_name)
+    return jsonify(data)
 @app.route('/send/<movie_name>', methods=["GET"])
 def get(movie_name):
     if request.method=="GET":
