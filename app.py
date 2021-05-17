95f0ac10d12e46308504c6fcc2b02b8b88976532
Jashwanth Bheemapaka <65611955+jashwanth999@users.noreply.github.com>
2021-05-18 00:16:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -112,7 +112,7 @@ def get_recommendations(title):
     idx=indices[title]
     sim_scores = list(enumerate(cosine_sim[idx]))
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    sim_scores = sim_scores[1:10]
+    sim_scores = sim_scores[1:15]
     movie_indices = [i[0] for i in sim_scores]
     return df2['title_x'].iloc[movie_indices]
 def get_data2(x):
